157044a935ff3b2bb122b3ac650bb157fd2af405
andres cabezas leiva <126105074+AcabezasLeiva@users.noreply.github.com>
2023-05-16 23:44:48 -0500

    Add files via upload

---
+++ ejercicioleccion2.js
@@ -0,0 +1,12 @@
+let miNombre= "andres mauricio cabezas leiva";
+let edad= "29";
+let soyDesarrollador= "false";
+const fechadeNacimiento= new Date (1993, 7, 12);
+
+
+let libro= {
+    titulo: "desaparecido",
+    autor: "Thomas Hauser" ,
+    fecha: "1982",
+    url: "https://librotea.com/libros/desaparecido-v1ldjbuulm/",
+}
